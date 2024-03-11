33c703b2c6aeb2657821183e3e77d9d611f888cf
unknown <alber1897@gmail.com>
2024-03-11 19:06:42 +0100

    a

--- e01_holaMundo/src/e01_holaMundo/holaMundo.java
+++ e01_holaMundo/src/e01_holaMundo/holaMundo.java
@@ -1,7 +1,10 @@
 package e01_holaMundo;
 
 public class holaMundo {
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("a");
 	}
+
 }
